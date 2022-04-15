ef4e108233c9990e09f3fd73f5142c407dc302c8
Brithnie Estime <estimebrithnie@yahoo.com>
2022-04-15 08:11:16 -0700

    Created an image folder and added an image. (js)Switched to jquerry(diplay current day), created a few html element through js(jquerry) and created a function for save button. Made few comments.

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,21 @@
+// Importing/calling moment js
 let currentDay = moment();
 
-document.querySelector('#currentDay').append(currentDay.format('dddd, MMMM Do'));
+// Display current time (header)
+$('#currentDay').append(currentDay.format('dddd, MMMM Do'));
+
+// Create html elements
+let rows = $('<div>').addclass('work-scheduler-rows');
+let workNotesBlock = $('<div>').addclass('col-10');
+let noteText = $('<textarea>');
+let timeBlokcs = $('<di>').addclass('col-1');
+let saveBtnContainer = $('<div>').addclass('col-1');
+let saveBtn = $('<button>').addclass('save-btn');
+
+// Function that incorporate the image and render saveBtn functional
+function saveButtonFunc() {
+    let img = $("<img src='./assets/images/saveBtn.png'>");
+    img.width(30);
+    saveBtn.attr('type', 'submit');
+    saveBtn.html(img);
+}
