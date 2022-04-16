55686c581d969c77add4376045eb4c75e8caa233
Brithnie Estime <estimebrithnie@yahoo.com>
2022-04-15 21:13:48 -0700

    Debugged (in trying to get the time to work(in accordance to the if statement(past,current,future)) and created an interval to update ate and time in html.

--- assets/script.js
+++ assets/script.js
@@ -2,15 +2,17 @@
 let currentDay = moment();
 
 // Display current day (header)
-$('#currentDay').append(currentDay.format('dddd, MMMM Do'));
+$('#currentDay').append(currentDay.format('dddd, MMMM Do, h:mm a'));
 
 // Create html elements
-let rows = $('<div>').addclass('work-scheduler-rows');
-let workNotesBlock = $('<div>').addclass('col-10');
+let rows = $('<div>').addClass('work-scheduler-rows');
+let workNotesBlocks = $('<div>').addClass('col-10');
 let noteText = $('<textarea>');
-let timeBlokcs = $('<di>').addclass('col-1 hour');
-let saveBtnContainer = $('<div>').addclass('col-1');
-let saveBtn = $('<button>').addclass('save-btn');
+let timeBlokcs = $('<div>').addClass('col-1 hour');
+let saveBtnContainer = $('<div>').addClass('col-1');
+let saveBtn = $('<button>').addClass('save-btn');
+
+// console.log(rows);
 
 // Function that incorporate the image and render saveBtn functional
 function saveButtonFunc() {
@@ -18,35 +20,45 @@ function saveButtonFunc() {
     img.width(30);
     saveBtn.attr('type', 'submit');
     saveBtn.html(img);
+    return saveBtn;
 }
 
 // Function that brings the created html elements to live
 function initWorkDaySchedProp() {
+    let currentTime = moment().format('h:00a');
     // Using for to initialize (set conditions & increment)
     for (let i = 9; i < 18; i++) {
         // Time block
-        let rowTime = moment().hour(i).format('HH:00 a');
+        let rowTime = moment().hour(i).format('h:00a');
         // Assigning ids to facilitate saving data to local storage
         noteText.attr('id', `hour-${i}`);
         timeBlokcs.text(rowTime);
         // if statement to compare the current time to the blocks assigned time
-        let currentTime = moment().format('HH:00 a')
         if (currentTime === rowTime) {
             // Assigning classes to be used in stylesheet(assigning background colors) base on the comparison results
-            workNotesBlock.addclass('current');
-            noteText.addclass('current');
+            workNotesBlocks.addClass('current');
+            noteText.addClass('current');
         } else if (currentTime > rowTime){
-            workNotesBlock.addclass('past');
-            noteText.addclass('past');
-        } else if (currentTime < rowTime) {
-            workNotesBlock.addclass('future');
-            noteText.addclass('future');
+            workNotesBlocks.addClass('past');
+            noteText.addClass('past');
+        } else {
+            workNotesBlocks.addClass('future');
+            noteText.addClass('future');
         }
         // Appending notes/text to container
-        workNotesBlock.append(noteText);
+        workNotesBlocks.append(noteText);
         // Appending variable(divs), afected by time, to rows(var/time container)
-        rows.append(timeBlokcs, workNotesBlock);
+        rows.append(timeBlokcs, workNotesBlocks, saveBtnContainer);
         // Appeding rows to inner html
         $('.container').append(rows);
+        // Append save button function to html created element saveBtnContainer
+        saveBtnContainer.append(saveButtonFunc());
     }
 }
+initWorkDaySchedProp();
+
+// To update html date and time
+setInterval(() => {
+    $('#currentDay').html(currentDay.format('dddd, MMMM Do, h:mm a'));
+}, 1000);
+
