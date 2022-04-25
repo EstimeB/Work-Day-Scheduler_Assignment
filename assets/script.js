304a64a2884ce92bc1b2c793f460bec9cd40acbd
Brithnie Estime <estimebrithnie@yahoo.com>
2022-04-24 19:19:03 -0700

    Had to relocate/rearrange a few line, especially the created htlm elemnets since the had to be within the for loop in order to have all the neccessary rows (multiple rows).

--- assets/script.js
+++ assets/script.js
@@ -4,30 +4,29 @@ let currentDay = moment();
 // Display current day (header)
 $('#currentDay').append(currentDay.format('dddd, MMMM Do, h:mm a'));
 
-// Create html elements
-let rows = $('<div>').addClass('row');
-let workNotesBlocks = $('<div>').addClass('col-10');
-let noteText = $('<textarea>');
-let timeBlokcs = $('<div>').addClass('col-1 hour time');
-let saveBtnContainer = $('<div>').addClass('col-1');
-let saveBtn = $('<button>').addClass('saveBtn');
-
-// console.log(rows);
-
-// Function that incorporate the image and render saveBtn functional
-function saveButtonFunc() {
-    let img = $("<img src='./assets/images/saveBtn.png'>");
-    img.width(20);
-    saveBtn.attr('type', 'submit');
-    saveBtn.html(img);
-    return saveBtn;
-}
-
 // Function that brings the created html elements to live
 function initWorkDaySchedProp() {
+    // Function that incorporate the image and render saveBtn functional
+    function saveButtonFunc() {
+        let saveBtn = $('<button>').addClass('saveBtn');
+        let img = $("<img src='./assets/images/saveBtn.png'>");
+        img.width(20);
+        saveBtn.attr('type', 'submit');
+        saveBtn.html(img);
+        return saveBtn;
+    }
+
     let currentTime = moment().format('h:00a');
     // Using for to initialize (set conditions & increment)
     for (let i = 9; i < 18; i++) {
+
+        // Create html elements
+        let rows = $('<div>').addClass('row');
+        let workNotesBlocks = $('<div>').addClass('col-10');
+        let noteText = $('<textarea>');
+        let timeBlokcs = $('<div>').addClass('col-1 hour time');
+        let saveBtnContainer = $('<div>').addClass('col-1');
+
         // Time block
         let rowTime = moment().hour(i).format('h:00a');
         // Assigning ids to facilitate saving data to local storage
@@ -38,7 +37,7 @@ function initWorkDaySchedProp() {
             // Assigning classes to be used in stylesheet(assigning background colors) base on the comparison results
             workNotesBlocks.addClass('current');
             noteText.addClass('current');
-        } else if (currentTime > rowTime){
+        } else if (currentTime > rowTime) {
             workNotesBlocks.addClass('past');
             noteText.addClass('past');
         } else {
@@ -54,11 +53,12 @@ function initWorkDaySchedProp() {
         // Append save button function to html created element saveBtnContainer
         saveBtnContainer.append(saveButtonFunc());
     }
+    // To update html date and time
+    setInterval(() => {
+        $('#currentDay').html(currentDay.format('dddd, MMMM Do, h:mm a'));
+    }, 1000);
+
 }
 initWorkDaySchedProp();
 
-// To update html date and time
-setInterval(() => {
-    $('#currentDay').html(currentDay.format('dddd, MMMM Do, h:mm a'));
-}, 1000);
 
